ac305f74615a82a0730dac432c111648ef29cef4
Stéphan Lascar <stephan@lascar.im>
2020-04-19 20:15:27 +0200

    Création d'un loader vuejs -- SLA

---
+++ src/components/Loader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="main-loader">
+        <div class="h-100 w-100 d-flex justify-content-center align-items-center">
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "loader"
+    }
+</script>
